cb1c4209d169249d794d9d8339cc37f778f850f7
mohammedelbakkali <120609888+mohammedelbakkali@users.noreply.github.com>
2024-04-18 01:44:49 +0100

    init

---
+++ src/app/components/light-info/light-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-light-info',
+  templateUrl: './light-info.component.html',
+  styleUrls: ['./light-info.component.scss']
+})
+export class LightInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
